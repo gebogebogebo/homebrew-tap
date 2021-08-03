5207d4b669bb9962f5f984e87c1ba452edf203a8
gebo <gebogebogeboge@gmail.com>
2021-08-04 07:22:55 +0900

    ctapcli v0.0.6

--- Formula/ctapcli.rb
+++ Formula/ctapcli.rb
@@ -1,8 +1,8 @@
 class Ctapcli < Formula
   desc "This tool implements CTAP HID and can communicate with FIDO Authenticator."
   homepage "https://github.com/gebogebogebo/ctap-hid-fido2"
-  url "https://github.com/gebogebogebo/ctap-hid-fido2/releases/download/ctapcli_v0.0.5/ctapcli-0.0.5-x86_64-apple-darwin.tar.gz"
-  sha256 "c9fc07c26e4b7513531288d9a6606f76017b0e73b7e4b63d1c7a2b0dea7f7b3d"
+  url "https://github.com/gebogebogebo/ctap-hid-fido2/releases/download/ctapcli_v0.0.6/ctapcli-0.0.6-x86_64-apple-darwin.tar.gz"
+  sha256 "97aad7a46f4b55ff9b39b26e9c55aae06ec21e73cb06ebcbb4321a2f29b17b69"
   license "MIT"
 
   def install
